f70d851379557148f675f73849af119a53e0538a
igorsimakov <Simakoff10@mail.ru>
2021-09-18 17:06:08 +0300

    first commit

---
+++ src/main/java/ru/sberbank/assistantServer/AssistantServerApplication.java
@@ -0,0 +1,13 @@
+package ru.sberbank.assistantServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistantServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sberbank/assistantServer/AssistantServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sberbank.assistantServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
